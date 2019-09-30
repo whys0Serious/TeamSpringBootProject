6c487b8153de38c66dd0e94d82b27faea1ea6916
18893075747 <1358616552@qq.com>
2019-09-30 15:16:10 +0800

    登陆注册逻辑 邮箱和用户管理员所有登陆业务的实现 mawencai

--- SpringBootProject/src/main/java/com/qf/domain/User.java
+++ SpringBootProject/src/main/java/com/qf/domain/User.java
@@ -2,16 +2,18 @@ package com.qf.domain;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Data
-@Table(name = "test")
 public class User {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String name;
-
+    @GeneratedValue//生成自增主键
+    private Integer uid;
+    private String uname;
+    private String email;//邮箱账号
+    private char sex;//用单选框,存储一个字节
+    private String pass;
 }
