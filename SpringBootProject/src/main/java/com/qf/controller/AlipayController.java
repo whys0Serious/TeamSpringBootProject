81c138484a9f126435ddf5f477b571d782cde22a
86156 <547303896@qq.com>
2019-10-11 21:15:07 +0800

    定时删除未支付订单

--- SpringBootProject/src/main/java/com/qf/controller/AlipayController.java
+++ SpringBootProject/src/main/java/com/qf/controller/AlipayController.java
@@ -14,6 +14,7 @@ import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
--- SpringBootProject/src/main/java/com/qf/utils/DeleteEmailCode.java
+++ SpringBootProject/src/main/java/com/qf/utils/DeleteEmailCode.java
@@ -1,6 +1,7 @@
 package com.qf.utils;
 
 import com.qf.dao.EmailRepository;
+import com.qf.dao.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,8 +14,15 @@ import java.util.Date;
 public class DeleteEmailCode {
     @Autowired
     private EmailRepository repository;
+    @Autowired
+    private OrderRepository orderRepository;
     @Scheduled(cron="0 */1 * * * ?")
     public void task(){
         System.out.println(new Date()+":共删除了"+repository.deleteByStatus((byte)0)+"条垃圾验证码");
+    }
+
+    @Scheduled(cron="0 */5 * * * ?")
+    public void deleteOrderNotPay(){
+        System.out.println(new Date()+":已删除"+orderRepository.deleteByTradstatus("待支付")+"条未支付订单");
     }
 }
